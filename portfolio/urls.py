9b6d2c5c0f1618b02567d6b9ad433aa8b285343c
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2020-05-07 18:29:23 +0530

    project completed

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('jobs.urls'),name = 'home'),
+    path('blog/',include('blog.urls'),name = 'blog'),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
